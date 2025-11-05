549478168ff0c9d19e4e61e45799789cad67d4b5
TEUNGKU-ZULKIFLI <teungkuzulkifli08@gmail.com>
2025-11-05 13:54:14 +0700

    refactor(phase-first): 🎯 Last touch before Phase Second — Sentuhan terakhir sebelum loncatan fase berikutnya

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <Arduino.h>
 
+// Di board Lolin D1 R1, LED_BUILTIN terhubung ke pin D4 (GPIO2)
+// Kita bisa pakai konstanta LED_BUILTIN karena kita sudah menentukan
+// 'board = lolin_d1_r1' di platformio.ini
+
 void setup() {
-  // Buka jalur komunikasi serial di 115200 bps
   Serial.begin(115200);
-
-  // Beri jeda 1 detik agar serial monitor siap
   delay(1000); 
-
   Serial.println("\n\n--- TEST ---");
-  Serial.println("Halo dari Lolin D1 R1!");
-  Serial.println("Fase 1, Langkah 2: Serial Monitor OK.");
-  Serial.println("Board sudah bangun dan siap menerima perintah.");
+  Serial.println("Fase 1, Langkah 4: Tes LED Internal.");
+
+  // Set pin LED sebagai OUTPUT
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("LED D4 (LED_BUILTIN) diset sebagai OUTPUT.");
 }
 
 void loop() {
-  // Biarkan kosong dulu. Kita hanya ingin tes setup.
+  Serial.println("LED Nyala!");
+  digitalWrite(LED_BUILTIN, LOW);   // LED internal Wemos/Lolin nyala saat LOW
+  delay(1000);                     // Tunggu 1 detik
+
+  Serial.println("LED Mati!");
+  digitalWrite(LED_BUILTIN, HIGH);  // LED internal Wemos/Lolin mati saat HIGH
+  delay(1000);                     // Tunggu 1 detik
 }
